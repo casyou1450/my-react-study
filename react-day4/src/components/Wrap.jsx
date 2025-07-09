2fa2fa9c1ed984d5625210ba12a1e253438db971
casyou1450 <155709412+casyou1450@users.noreply.github.com>
2025-07-10 08:29:44 +0900

    Update Wrap.jsx

--- react-day4/src/components/Wrap.jsx
+++ react-day4/src/components/Wrap.jsx
@@ -1,33 +1,34 @@
-import Refund from './Refund'
+import Amount from './Amount'
 import React from 'react'
 import { useState } from 'react'
 import '../css/layout.css'
 
 const Wrap = () => {
   const [amount,setAmounts]=useState({
+    target:'',
     krw:'', 
     usd:'',  
   })
   
   // KRW 입력 시: USD = KRW * 1300
-  const handleKrw = (value) => {
-    setAmounts({
-      krw: value,
-      usd: Number(value) / 1300,
-    })
-  }
-  
   // USD 입력 시: KRW = USD / 1300
-  const handleUsd = (value) => {
-    setAmounts({
-      krw: Number(value) * 1300,
-      usd: value
-    })
+  const handleAmount = (value,name) => {
+    if(name==="krw"){
+      setAmounts({
+        krw: value,
+        usd: Number(value) / 1300,
+      })
+    }else{
+      setAmounts({
+        krw: Number(value) * 1300,
+        usd: value
+      })
+    }
   }
-  
+
   return (
     <div className='wrap'>
-        <Refund amount={amount} handleKrw={handleKrw} handleUsd={handleUsd}/>
+        <Amount amount={amount} handleAmount={handleAmount}/>
     </div>
   )
 }
