9b394211b6e9020874bde6eded2a7322b653bc5e
PYK <t2405081@partner.coway.com>
2025-07-14 17:38:20 +0900

    react_address 프로젝트 추가

---
+++ react_address/src/components/ContactItem.jsx
@@ -0,0 +1,11 @@
+import "../css/ContactItem.css";
+
+export default function ContactItem() {
+  return (
+    <div className="ContactItem">
+      <div className="name">이정환</div>
+      <div className="contact">king199777@gmail.com</div>
+      <button>🗑️ Remove</button>
+    </div>
+  );
+}
---
+++ react_address/src/components/ContactList.jsx
@@ -0,0 +1,13 @@
+import "../css/ContactList.css";
+import ContactItem from "./ContactItem";
+
+export default function ContactList() {
+  return (
+    <div className="ContactList">
+      <div className="title">Contact List</div>
+      <ContactItem />
+      <ContactItem />
+      <ContactItem />
+    </div>
+  );
+}
