3db3904a297b8c7868121c3e493c821211bb2226
casyou1450 <155709412+casyou1450@users.noreply.github.com>
2025-08-19 17:27:00 +0900

    Update main.jsx with actual implementation

--- react_youtube/src/main.jsx
+++ react_youtube/src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import './css/reset.css'
+import './css/layout.css'
 import './index.css'
 import App from './App.jsx'
 
+const queryClient = new QueryClient({
+  defaultOptions:{
+    queries:{
+      staleTime:1000*60*5,
+      cacheTime:1000*60*10,
+    }
+  }
+})
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </StrictMode>
 )
